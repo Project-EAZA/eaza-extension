f37dad1a4cc37e00497747a46fd734cde32d13c6
Odysa <cbian7@wisc.edu>
2022-08-30 17:04:53 -0500

    chore: clean projects

--- src/entries/options/App.svelte
+++ src/entries/options/App.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
-import PageContent from "~/components/PageContent.svelte";
 </script>
 
-<main>
-  <PageContent> Options </PageContent>
-</main>
+<main />
 
 <style>
-main {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 </style>
