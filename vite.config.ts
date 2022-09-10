1b6268187e249d060f9cbd83e4b2adb5e287f246
Chengxu Bian <cbian7@wisc.edu>
2022-09-10 14:01:04 -0500

    chore: remove hash from build file names

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,14 @@ export default defineConfig(() => {
         "~": path.resolve(__dirname, "./src"),
       },
     },
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+        },
+      },
+    },
   };
 });
